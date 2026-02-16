84bf8578cfcf4b5860dd2cdff7e9386a7350aef5
Shreyas Auti <shreyasdeep253@gmail.com>
2026-02-16 19:28:04 +0530

    Update mlApi.ts

--- src/services/mlApi.ts
+++ src/services/mlApi.ts
@@ -1,9 +1,11 @@
+const BASE_URL = "https://hackowire-1.onrender.com";
+
 export async function fetchCustomers() {
-  const res = await fetch("http://127.0.0.1:8000/customers");
-  return await res.json();
+  const res = await fetch(`${BASE_URL}/customers`);
+  return res.json();
 }
 
-export async function fetchRisk(customerId: number) {
-  const res = await fetch(`http://127.0.0.1:8000/risk/${customerId}`);
-  return await res.json();
+export async function fetchRisk(id: number) {
+  const res = await fetch(`${BASE_URL}/risk/${id}`);
+  return res.json();
 }
